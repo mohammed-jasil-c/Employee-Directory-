c18440db9dc2cd10a63346dcbeb33fd90abc1959
mohammed-jasil-c <jasilchemmala@gmail.com>
2026-03-22 15:36:43 +0530

    Auto create admin user during build phase

--- backend/build.sh
+++ backend/build.sh
@@ -3,3 +3,5 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "Creating default admin if not exists..."
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@company.com').exists() or User.objects.create_superuser('admin@company.com', 'admin123')"
